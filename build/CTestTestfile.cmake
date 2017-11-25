fbf32e3e1c846c16300996c4b424c5cca6e55b26
Julien Gilbert <julien.gilbert@phelma.grenoble-inp.fr>
2017-11-25 07:33:40 +0100

    modification

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /user/2/.base/deshayec/home/Documents/SEPC/Tp Thread/ensimag-video
-# Build directory: /user/2/.base/deshayec/home/Documents/SEPC/Tp Thread/ensimag-video/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(CommonPthreadError "perl" "/user/2/.base/deshayec/home/Documents/SEPC/Tp Thread/ensimag-video/tests/pthcocci.pl")
--- src/synchro.c
+++ src/synchro.c
@@ -7,9 +7,13 @@ bool fini;
 /* les variables pour la synchro, ici */
 pthread_mutex_t m;
 pthread_mutex_t m_texture;
+pthread_mutex_t m_consommation;
 pthread_cond_t image;
 pthread_cond_t c_texture;
+pthread_cond_t consommation_texture;
 bool fenetrePrete = false;
+
+int nombreTextureDeposee = 0; //compte le nombre de texture qui sont déposées mais qui n'ont pas été consommé
 
 /* l'implantation des fonctions de synchro ici */
 void envoiTailleFenetre(th_ycbcr_buffer buffer) {
@@ -45,26 +49,47 @@ void attendreFenetreTexture() {
 }
 
 void debutConsommerTexture() {
+  pthread_mutex_lock(&m_consommation);
+  
+  while(nombreTextureDeposee == 0){
+    pthread_cond_wait(&consommation_texture, &m_consommation);
+  }
+  
+  nombreTextureDeposee --;
+  pthread_mutex_unlock(&m_consommation);
 
 }
 
 void finConsommerTexture() {
+  pthread_mutex_lock(&m_consommation);
+  
+  if(nombreTextureDeposee < NBTEX) {
+    c_texture.signal(); //on reveille un thread qui depose de la texture
+  }
+  
+  pthread_mutex_unlock(&m_consommation);
 }
 
 
 void debutDeposerTexture() {
   pthread_mutex_lock(&m_texture);
-  // while (le tampon est vide){
-  pthread_cond_wait(&c_texture, &m_texture);
-  //}
-  
+  while (nombreTextureDeposee > NBTEX){
+    pthread_cond_wait(&c_texture, &m_texture);
+  }
+
+  nombreTextureDeposee ++;
+ 
   pthread_mutex_unlock(&m_texture);
 }
 
 void finDeposerTexture() {
   pthread_mutex_lock(&m_texture);
- 
- 
   
+  if(nombreTextureDeposee > NBTEX) {
+    consommation_texture.broadcast(); //on peut consommer de la texture
+  }
+  else {
+    c_texture.signal(); //on peut deposer de la texture
+  }
   pthread_mutex_unlock(&m_texture);
 }
